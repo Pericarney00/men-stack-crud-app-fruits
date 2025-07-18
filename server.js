a982e306809b1c53c26525955bcfb014606cbd4d
pericarney00 <pericarney@gmail.com>
2025-07-18 11:30:14 -0500

    created an index.ejs file and created a functioning route to it.

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express")
 
 const app = express();
 
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs")
+});
+
+
+
+
+
 app.listen(3000, () => {
   console.log("Listening on port 3000")
 });
